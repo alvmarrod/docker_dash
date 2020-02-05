0fa3fe9cae0e66aba233d4fdf37e7c419a08ff98
Alvaro MR <btc@hotmail.es>
2020-02-05 14:34:45 +0100

    Existing images are shown on web interface

--- ldd/backend/main.go
+++ ldd/backend/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+    "log"
     "fmt"
     "time"
+    "strings"
     // "strconv"
+    "os/exec"
     "net/http"
     // "math/rand"
     "encoding/json"
@@ -23,10 +26,79 @@ const (
 
 var DEBUG_LEVEL debugLevel = Info
 
-func writeLog(msg string, level debugLevel){
+func logEvent(msg string, level debugLevel){
     if (level >= DEBUG_LEVEL){
         fmt.Printf("%s - %s\n", time.Now().Format(time.RFC850), msg)
     }
+}
+
+func logDetail(msg string, level debugLevel){
+    if (level >= DEBUG_LEVEL){
+        fmt.Printf("%s\n%s\n", time.Now().Format(time.RFC850), msg)
+    }
+}
+
+/* External Invocation Functions */
+func runOnHost(cmd string) string {
+
+    parts := strings.Fields(cmd)
+    app := parts[0]
+    args := parts[1:len(parts)]
+
+    // Add format settings after Fields command
+    if args[0] == "image" && args[1] == "ls" {
+        args = append(args, "--format")
+        args = append(args, `{{.Repository}}\t{{.Tag}}\t{{.ID}}\t{{.CreatedAt}}\t{{.Size}}`)
+    }
+
+    out, err := exec.Command(app, args...).Output()
+    // logDetail(string(out), Critical)
+
+    if (err != nil) {
+        logEvent("Error", Critical)
+        log.Fatal(err)
+        out = []byte("Error")
+    }
+
+    return string(out)
+
+}
+
+func parseDockerImages(data string) {
+
+    lines := strings.Split(data, "\n")
+
+    // Remove previous data
+    images = nil
+
+    // Line Format (split by \t)
+    // Repository Tag Id Created Size
+    for i:=0; i < len(lines) && len(lines[i]) > 0; i++ {
+        
+        piece := strings.Split(lines[i], "\t")
+
+        // logEvent("Line " + string(i) + " = " + lines[i], Critical)
+        // for j:=0; j < len(piece); j++ {
+        //     logEvent("\t" + piece[j], Critical)
+        // }
+        // logEvent(lines[i], Critical)
+        
+        // Add new item to array
+        var image DockerImage
+        image.Repository = piece[0]
+        image.TAG = piece[1]
+        image.ID = piece[2]
+        image.Created = piece[3]
+        image.Size = piece[4] // strconv.ParseInt(, 10, 64)
+
+        images = append(images, image)
+
+    }
+
+}
+
+func parseDockerContainers(data string) {
+
 }
 
 /* Enable Cors */
@@ -47,7 +119,7 @@ type DockerImage struct {
     TAG string `json:"tag"`
     ID string `json:"id"`
     Created string `json:"created"`
-    Size int64 `json:"size"`    // In Bytes
+    Size string `json:"size"`
 }
 
 type DockerContainer struct {
@@ -65,13 +137,23 @@ var containers []DockerContainer
 
 /* Get complete list of items */
 
-func getImages(w http.ResponseWriter, r *http.Request) {
+func reqGetDockerImages(w http.ResponseWriter, r *http.Request) {
+    // Update images availables
+    output := runOnHost("docker image ls")
+    parseDockerImages(output)
+
+    // Send response
     w.Header().Set("Content-Type", "application/json")
     enableCors(&w)
     json.NewEncoder(w).Encode(images)
 }
 
-func getContainers(w http.ResponseWriter, r *http.Request) {
+func reqGetDockerContainers(w http.ResponseWriter, r *http.Request) {
+    // Update images availables
+    output := runOnHost("docker container ls")
+    parseDockerContainers(output)
+
+    // Send response
     w.Header().Set("Content-Type", "application/json")
     enableCors(&w)
     json.NewEncoder(w).Encode(containers)
@@ -79,7 +161,7 @@ func getContainers(w http.ResponseWriter, r *http.Request) {
 
 /* Get specific item */
 
-func getImage(w http.ResponseWriter, r *http.Request) {
+func reqGetImage(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
     enableCors(&w)
     params := mux.Vars(r)
@@ -93,7 +175,7 @@ func getImage(w http.ResponseWriter, r *http.Request) {
     // json.NewEncoder(w).Encode(&DockerImage{})
 }
 
-func getContainer(w http.ResponseWriter, r *http.Request) {
+func reqGetDockerContainer(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
     enableCors(&w)
     params := mux.Vars(r)
@@ -108,7 +190,7 @@ func getContainer(w http.ResponseWriter, r *http.Request) {
 
 /* Create New Item */
 
-func createDockerImage(w http.ResponseWriter, r *http.Request) {
+func reqCreateDockerImage(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
     enableCors(&w)
     var image DockerImage
@@ -127,7 +209,7 @@ func createDockerImage(w http.ResponseWriter, r *http.Request) {
     
 }
 
-func createDockerContainer(w http.ResponseWriter, r *http.Request) {
+func reqCreateDockerContainer(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
     enableCors(&w)
     var container DockerContainer
@@ -147,7 +229,7 @@ func createDockerContainer(w http.ResponseWriter, r *http.Request) {
 }
 
 /* Update Item - Execute action over it */
-func updateImage(w http.ResponseWriter, r *http.Request) {
+func reqUpdateImage(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
     enableCors(&w)
     params := mux.Vars(r)
@@ -174,7 +256,7 @@ func updateImage(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func updateContainer(w http.ResponseWriter, r *http.Request) {
+func reqUpdateContainer(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
     enableCors(&w)
     params := mux.Vars(r)
@@ -201,7 +283,7 @@ func updateContainer(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func deleteImage(w http.ResponseWriter, r *http.Request) {
+func reqDeleteImage(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
     enableCors(&w)
     params := mux.Vars(r)
@@ -220,7 +302,7 @@ func deleteImage(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func deleteContainer(w http.ResponseWriter, r *http.Request) {
+func reqDeleteContainer(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
     enableCors(&w)
     params := mux.Vars(r)
@@ -241,33 +323,33 @@ func deleteContainer(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-    images = append(images, DockerImage{Repository: "prueba/imagen1", TAG: "23984723894723984", ID: "1", Created: "25/01/2020", Size: 255})
-    images = append(images, DockerImage{Repository: "prueba/imagen2", TAG: "23984723894723984", ID: "2", Created: "25/01/2020", Size: 255})
-    images = append(images, DockerImage{Repository: "prueba/imagen3", TAG: "23984723894723984", ID: "3", Created: "25/01/2020", Size: 255})
-    images = append(images, DockerImage{Repository: "prueba/imagen4", TAG: "23984723894723984", ID: "4", Created: "25/01/2020", Size: 255})
+    images = append(images, DockerImage{Repository: "prueba/imagen1", TAG: "23984723894723984", ID: "1", Created: "25/01/2020", Size: "255KB"})
+    images = append(images, DockerImage{Repository: "prueba/imagen2", TAG: "23984723894723984", ID: "2", Created: "25/01/2020", Size: "255KB"})
+    images = append(images, DockerImage{Repository: "prueba/imagen3", TAG: "23984723894723984", ID: "3", Created: "25/01/2020", Size: "255KB"})
+    images = append(images, DockerImage{Repository: "prueba/imagen4", TAG: "23984723894723984", ID: "4", Created: "25/01/2020", Size: "255KB"})
 
     router := mux.NewRouter()
 
     /* Get complete list of items */
-    router.HandleFunc("/images", getImages).Methods("GET")
-    router.HandleFunc("/containers", getContainers).Methods("GET")
+    router.HandleFunc("/images", reqGetDockerImages).Methods("GET")
+    router.HandleFunc("/containers", reqGetDockerContainers).Methods("GET")
 
     /* Get specific item */
-    router.HandleFunc("/images/{id}", getImage).Methods("GET")
-    router.HandleFunc("/containers/{id}", getContainer).Methods("GET")
+    router.HandleFunc("/images/{id}", reqGetImage).Methods("GET")
+    router.HandleFunc("/containers/{id}", reqGetDockerContainer).Methods("GET")
 
     /* Create New Item */
-    router.HandleFunc("/images", createDockerImage).Methods("POST")
-    router.HandleFunc("/containers", createDockerContainer).Methods("POST")
+    router.HandleFunc("/images", reqCreateDockerImage).Methods("POST")
+    router.HandleFunc("/containers", reqCreateDockerContainer).Methods("POST")
 
     /* Update Item - Execute action over it */
-    router.HandleFunc("/images/{id}", updateImage).Methods("PUT")
-    router.HandleFunc("/containers/{id}", updateContainer).Methods("PUT")
+    router.HandleFunc("/images/{id}", reqUpdateImage).Methods("PUT")
+    router.HandleFunc("/containers/{id}", reqUpdateContainer).Methods("PUT")
 
-    router.HandleFunc("/images/{id}", deleteImage).Methods("DELETE")
-    router.HandleFunc("/containers/{id}", deleteContainer).Methods("DELETE")
-    
-    writeLog("Listening", Info)
+    router.HandleFunc("/images/{id}", reqDeleteImage).Methods("DELETE")
+    router.HandleFunc("/containers/{id}", reqDeleteContainer).Methods("DELETE")
+
+    logEvent("Listening", Info)
     http.ListenAndServe(":8000", router)
 
 
