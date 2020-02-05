ecf43d272451acbf480f4740d0b211a9af828288
Alvaro MR <btc@hotmail.es>
2020-02-05 16:19:45 +0100

    Fixes on JSX for Stopped Containers component

--- ldd/src/StoppedContainers.js
+++ ldd/src/StoppedContainers.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from './Button';
 import DockerContainer from './DockerContainer';
 
 import './Module.css';
@@ -28,24 +29,37 @@ class StoppedContainers extends React.Component {
           <span className="Module-title">
             <img src={docker_logo} className="Module-logo" alt="logo" />
             Stopped Containers
-                </span>
+          </span>
           <br />
-          <table>
-            <thead>
-              <tr className="Table-header">
-                <th>Name</th>
-                <th>Container ID</th>
-                <th>Image</th>
-                <th>CMD</th>
-                <th>Created</th>
-                <th>Status</th>
-                <th>Ports</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.renderContainers()}
-            </tbody>
-          </table>
+          <div className="Table-Wrapper">
+            <table>
+              <thead>
+                <tr className="Table-header">
+                  <th>Name</th>
+                  <th>Container ID</th>
+                  <th>Image</th>
+                  <th>CMD</th>
+                  <th>Created</th>
+                  <th>Status</th>
+                  <th>Ports</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="Regular-Row">
+                  <th>Col Data 1</th>
+                  <th>Col Data 2</th>
+                  <th>Col Data 3</th>
+                  <th>Col Data 4</th>
+                  <th>Col Data 5</th>
+                  <th>Col Data 6</th>
+                  <th>Col Data 7</th>
+                  <th><Button title="Start" /></th>
+                </tr>
+                {this.renderContainers()}
+              </tbody>
+            </table>
+          </div>
         </header>
       </div>
     );
