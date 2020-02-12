379a5d48dac7a729c609df9a0d8d143d6dc46efc
Alvaro MR <btc@hotmail.es>
2020-02-12 16:03:44 +0100

    Added remove image button on interface

--- ldd/src/DockerImage.js
+++ ldd/src/DockerImage.js
@@ -7,12 +7,13 @@ class DockerImage extends React.Component{
 
         return (
             <tr className="Regular-Row">
+                
                 <th>{this.props.image.repository}</th>
                 <th>{this.props.image.tag}</th>
                 <th>{this.props.image.id}</th>
                 <th>{this.props.image.created}</th>
                 <th>{this.props.image.size}</th>
-                <th><Button title="Run" className="Run" /></th>
+                <th><Button title="Run" className="Run" /><Button title="X" className="Stop" /></th>
             </tr>
         )
 
