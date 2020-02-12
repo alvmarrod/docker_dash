ad34b21c21d315f003a7884eb7a51419ca51655a
Alvaro MR <btc@hotmail.es>
2020-02-12 16:28:11 +0100

    Updated Docker Container component to render conditional column based on prop

--- ldd/src/DockerContainer.js
+++ ldd/src/DockerContainer.js
@@ -1,42 +1,57 @@
 import React from 'react';
 import Button from './Button';
 
-class DockerContainer extends React.Component{
+class DockerContainer extends React.Component {
 
-    apiQueryForButton(){
+  renderConditionalColumn(col_name) {
 
-        var query = "";
+    switch (col_name) {
+      case "Created":
 
-        if (this.props.button["title"] === "Stop") {
-            query = "StopContainer";            
-        } else if (this.props.button["title"] === "Start") {
-            query = "StartContainer";
-        } else {
-            console.log("Error, unknown button title: " + this.props.button["title"]);
+        if (this.props.show_cdate) {
+          return <th>{this.props.container.created}</th>
         }
-
-        return query;
-    }
-
-    render(){
-
-        return (
-            <tr className="Regular-Row">
-                <th>{this.props.container.name}</th>
-                <th>{this.props.container.id}</th>
-                <th>{this.props.container.image}</th>
-                <th>{this.props.container.cmd}</th>
-                <th>{this.props.container.created}</th>
-                <th>{this.props.container.status}</th>
-                <th>{this.props.container.ports}</th>
-                <th><Button title={this.props.button["title"]}
-                            className={this.props.button["className"]}
-                            apiQuery={this.apiQueryForButton()}
-                            itemID={this.props.container.id} /></th>
-            </tr>
-        )
+        break;
+      default:
 
     }
+
+  }
+
+  apiQueryForButton() {
+
+    var query = "";
+
+    if (this.props.button["title"] === "Stop") {
+      query = "StopContainer";
+    } else if (this.props.button["title"] === "Start") {
+      query = "StartContainer";
+    } else {
+      console.log("Error, unknown button title: " + this.props.button["title"]);
+    }
+
+    return query;
+  }
+
+  render() {
+
+    return (
+      <tr className="Regular-Row">
+        <th>{this.props.container.name}</th>
+        <th>{this.props.container.id}</th>
+        <th>{this.props.container.image}</th>
+        <th>{this.props.container.cmd}</th>
+        {this.renderConditionalColumn("Created")}
+        <th>{this.props.container.status}</th>
+        <th>{this.props.container.ports}</th>
+        <th><Button title={this.props.button["title"]}
+                    className={this.props.button["className"]}
+                    apiQuery={this.apiQueryForButton()}
+                    itemID={this.props.container.id} /></th>
+      </tr>
+    )
+
+  }
 }
 
 export default DockerContainer;
